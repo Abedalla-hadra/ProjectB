4e140cafe6f8c3c653b46384d226a6eda9b81524
Abedalla-hadra <Abedalla.hadra@gmail.com>
2019-11-02 16:35:07 +0200

    adding a Point class

---
+++ Point.java
@@ -0,0 +1,31 @@
+package ProjectB;
+
+public class Point {
+	int y;
+	int x;
+	int z;
+	boolean discovered;
+	Point parent;
+	public Point(int _y, int _x, int _z) {
+		this.y = _y;
+		this.x = _x;
+		this.z = _z;
+		this.discovered = false;
+		this.parent = null;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getZ() {
+		return z;
+	}
+	public boolean isDiscovered() {
+		return discovered;
+	}
+	public Point getParent() {
+		return parent;
+	}
+}
